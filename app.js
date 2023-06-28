291301f561d30c8f2be687f65af289e54adfcd0b
Frankmona <frankoguche1@gmail.com>
2023-06-28 23:59:57 +0100

    completed project

---
+++ app.js
@@ -0,0 +1,11 @@
+const shareBTN = document.querySelector(".share");
+const section = document.querySelector(".section");
+
+shareBTN.addEventListener("click", function () {
+  //   console.log(shareBTN);
+  if (section.classList.contains("show-text")) {
+    section.classList.remove("show-text");
+  } else {
+    section.classList.add("show-text");
+  }
+});
