e80a6825fc09f847e8a3c3186e392f6750dd9265
Frankmona <frankoguche1@gmail.com>
2023-07-02 17:38:54 +0100

    completed media query

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const shareBTN = document.querySelector(".share");
 const section = document.querySelector(".section");
 
 shareBTN.addEventListener("click", function () {
-  //   console.log(shareBTN);
+  // console.log(shareBTN);
   if (section.classList.contains("show-text")) {
     section.classList.remove("show-text");
   } else {
